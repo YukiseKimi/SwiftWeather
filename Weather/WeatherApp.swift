f2d858f2faebc88db02f5a92a6d9bd314b441bdc
YukiseKimi <mannychau@gmail.com>
2021-10-10 16:53:19 +0100

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Manny Chau on 2021-10-10.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
